4c8ac21c4bc635966ae2cb00b09f38c374dc08eb
Yoann0000 <yoann.rohrbasser@bluewin.ch>
2019-04-13 16:27:23 +0200

    Update IConfigurationManager.java

--- src/mailBot/config/IConfigurationManager.java
+++ src/mailBot/config/IConfigurationManager.java
@@ -1,4 +1,11 @@
 package mailBot.config;
 
+import mailBot.model.mail.Person;
+
+import java.util.List;
+
 public interface IConfigurationManager {
+    List<Person> getVictims();
+
+    List<String> getMessages();
 }
