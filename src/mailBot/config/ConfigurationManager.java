c817d3385e34a9f67e63c3da94ec4217fc5e5b22
Yoann0000 <yoann.rohrbasser@bluewin.ch>
2019-04-13 16:27:09 +0200

    config manager

--- src/mailBot/config/ConfigurationManager.java
+++ src/mailBot/config/ConfigurationManager.java
@@ -1,4 +1,93 @@
 package mailBot.config;
 
-public class ConfigurationManager {
+import mailBot.model.mail.Person;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+public class ConfigurationManager implements IConfigurationManager{
+    private String smtpServerAddress;
+    private int smtpServerPort;
+    private final List<Person> victims;
+    private final List<String> messages;
+    private int numberOfGroups;
+    private List<Person> witnessesToCC;
+
+    public ConfigurationManager() throws IOException {
+        victims = loadAddressesFromFile("./config/victims.utf8");
+        messages = loadMessagesFromFile("./config/messages.utf8");
+        loadProperties("./config/config.properties");
+    }
+
+    private void loadProperties (String fileName) throws IOException {
+        FileInputStream fis = new FileInputStream(fileName);
+        Properties properties = new Properties();
+        properties.load(fis);
+
+        this.smtpServerAddress = properties.getProperty("smtpServerAddress");
+        this.smtpServerPort = Integer.parseInt(properties.getProperty("smtpServerPort"));
+        this.numberOfGroups = Integer.parseInt(properties.getProperty("numberOfGroups"));
+
+        this.witnessesToCC = new ArrayList<>();
+        String witnesses = properties.getProperty("witnessesToCC");
+        String[] witnessesAddresses = witnesses.split(",");
+        for (String address : witnessesAddresses) {
+            this.witnessesToCC.add(new Person(address));
+        }
+    }
+
+    private List<Person> loadAddressesFromFile(String fileName) throws IOException {
+        List<Person> result;
+        try (FileInputStream fis = new FileInputStream(fileName)) {
+            InputStreamReader isr = new InputStreamReader(fis, StandardCharsets.UTF_8);
+            try (BufferedReader reader = new BufferedReader(isr)) {
+                result = new ArrayList<>();
+                String address = reader.readLine();
+                while (address != null) {
+                    result.add(new Person(address));
+                    address = reader.readLine();
+                }
+            }
+        }
+        return result;
+    }
+
+    private List<String> loadMessagesFromFile(String fileName) throws IOException {
+        List<String> result;
+        try (FileInputStream fis = new FileInputStream(fileName)) {
+            InputStreamReader isr = new InputStreamReader(fis, StandardCharsets.UTF_8);
+            try (BufferedReader reader = new BufferedReader(isr)) {
+                result = new ArrayList<>();
+                String line = reader.readLine();
+                while (line != null) {
+                    StringBuilder body = new StringBuilder();
+                    while (!line.equals("==")) {
+                        body.append(line);
+                        body.append("\r\n");
+                        line = reader.readLine();
+                    }
+                    result.add(body.toString());
+                    line = reader.readLine();
+                }
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public List<Person> getVictims() {
+        return victims;
+    }
+
+    @Override
+    public List<String> getMessages() {
+        return messages;
+    }
+
 }
