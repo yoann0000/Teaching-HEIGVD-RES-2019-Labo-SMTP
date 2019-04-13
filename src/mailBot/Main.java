817604f30ffe6136e37c0f77ee965a6a97452630
Yoann0000 <yoann.rohrbasser@bluewin.ch>
2019-04-13 17:42:47 +0200

    Main

--- src/mailBot/Main.java
+++ src/mailBot/Main.java
@@ -1,8 +1,33 @@
 package mailBot;
 
+import mailBot.config.ConfigurationManager;
+import mailBot.config.IConfigurationManager;
+import mailBot.model.prank.Prank;
+import mailBot.model.prank.PrankGenerator;
+import mailBot.smtp.ISMTPClient;
+import mailBot.smtp.SMTPClient;
+
+import java.io.IOException;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        String smtpServerAddress = "address"; //FIXME
+        try {
+            IConfigurationManager configurationManager = new ConfigurationManager();
+            PrankGenerator prankGenerator = new PrankGenerator(configurationManager);
+
+            ISMTPClient smtpClient = new SMTPClient(smtpServerAddress, 25);
+
+            List<Prank> pranks = prankGenerator.generatePranks();
+
+            for (Prank prank : pranks) {
+                smtpClient.sendMessage(prank.generateMailMessage());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
